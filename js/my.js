64c9a18b89c8ebffb2ef0c26f4d087910506bf31
ramoni-adeogun <81020490+ramoni-adeogun@users.noreply.github.com>
2021-03-20 19:34:02 +0100

    Update my.js

--- js/my.js
+++ js/my.js
@@ -4,21 +4,21 @@ function home(){
    document.getElementById('home').style.display="inline";
    document.getElementById('publications').style.display="none";
    document.getElementById('teaching').style.display="none";
-   document.getElementById('fam').style.display="none";
+   document.getElementById('project').style.display="none";
 }
 function bio(){
    document.getElementById('bio').style.display="inline";
    document.getElementById('home').style.display="none";
    document.getElementById('publications').style.display="none";
    document.getElementById('teaching').style.display="none";
-   document.getElementById('fam').style.display="none";
+   document.getElementById('project').style.display="none";
 }
 function publications(){
    document.getElementById('bio').style.display="none";
    document.getElementById('home').style.display="none";
    document.getElementById('publications').style.display="inline";
    document.getElementById('teaching').style.display="none";
-   document.getElementById('fam').style.display="none";
+   document.getElementById('project').style.display="none";
 }
 
 function teaching(){
@@ -26,7 +26,7 @@ function teaching(){
    document.getElementById('home').style.display="none";
    document.getElementById('publications').style.display="none";
    document.getElementById('teaching').style.display="inline";
-   document.getElementById('fam').style.display="none";
+   document.getElementById('project').style.display="none";
 }
 
 
@@ -36,7 +36,7 @@ function fam(){
    document.getElementById('home').style.display="none";
    document.getElementById('publications').style.display="none";
    document.getElementById('teaching').style.display="none";
-   document.getElementById('fam').style.display="inline";
+   document.getElementById('project').style.display="inline";
 }
 
 
